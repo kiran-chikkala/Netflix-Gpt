7e4c35506629edf8363989279627cc306f85b645
chikkala kiran <saikiranchikkala20@gmail.com>
2023-11-22 15:22:44 +0530

    commit

--- src/components/GptMovieSuggestions.js
+++ src/components/GptMovieSuggestions.js
@@ -3,20 +3,23 @@ import { useSelector } from "react-redux";
 
 const GptMovieSuggestions = () => {
   const store = useSelector((store) => store?.gpt?.movieResults);
+
   if (!store) return;
   return (
     <div className=" bg-black w-screen h-full text-white grid  grid-cols-1 md:grid-cols-2 bg-opacity-50   ">
-      {store.map((item) => (
-        <div className="md:flex md:flex-col md:w-[500px]  px-[50px] py-5">
-          <img
-            className=" "
-            src={"https://image.tmdb.org/t/p/w500/" + item?.backdrop_path}
-            alt=""
-          />
-          <h1>{item.original_title}</h1>
-          <h1>{item.overview}</h1>
-        </div>
-      ))}
+      {store
+        .filter((item) => item?.backdrop_path !== null)
+        .map((item) => (
+          <div className="md:flex md:flex-col md:w-[500px]  px-[50px] py-5">
+            <img
+              className=" "
+              src={"https://image.tmdb.org/t/p/w500/" + item?.backdrop_path}
+              alt=""
+            />
+            <h1>{item.original_title}</h1>
+            <h1>{item.overview}</h1>
+          </div>
+        ))}
     </div>
   );
 };
